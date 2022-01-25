e5deb8de5deada9db0e97f9b779331e432ad4c0d
macsonm <75022784+macsonm@users.noreply.github.com>
2022-01-25 14:34:44 +0300

    Initial Commit

---
+++ FindNumber_icodeschool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindNumber_icodeschool
+//
+//  Created by Maxim M on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
