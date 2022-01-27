13ed38f449c838c1b02dae3f079514e2ac8102ec
macsonm <75022784+macsonm@users.noreply.github.com>
2022-01-27 16:20:22 +0300

    Timer, Animation, StackView

--- FindNumber_icodeschool/GameModel.swift
+++ FindNumber_icodeschool/GameModel.swift
@@ -10,6 +10,7 @@ import Foundation
 enum StatusGame{
     case start
     case win
+    case lose
 }
 
 class Game{
@@ -17,42 +18,92 @@ class Game{
     struct Item{
         var title: String
         var isFound: Bool = false
+        var isError = false
     }
     
     private let dataNumbers = Array(1...99)
     var itemsButtons: [Item] = []
     private var countItems: Int
     var nextItem: Item?
-    var status: StatusGame = .start
+    var status: StatusGame = .start{
+        didSet{
+            if status != .start{
+                stopGame()
+            }
+        }
+    }
+   
+    private var timerForGame: Int
+    private var secondsGame: Int{
+        didSet{
+            if secondsGame == 0 {
+                status = .lose
+            }
+            updateTimer(status,secondsGame)
+        }
+    }
+    private var timer: Timer?
+    private var updateTimer:((StatusGame,Int)->Void)
     
-    init(countItems: Int){
+    init(countItems: Int, time: Int, updateTimer: @escaping (_ status: StatusGame,_ seconds: Int) -> Void){
         self.countItems = countItems
+        self.timerForGame = time
+        self.secondsGame = time
+        self.updateTimer = updateTimer
+        
         setupGame()
     }
     
     
     private func setupGame() {
         var digits = dataNumbers.shuffled()
-        
+        itemsButtons.removeAll()        //очистка номерков для кнопочек
         while itemsButtons.count < countItems {
             let item = Item(title: String(digits.removeFirst()))
             itemsButtons.append(item)       // создали цифры для кнопок
         }
         
         nextItem = itemsButtons.shuffled().first    //рандомим цифры, которые будут выводиться на кнопках
+        updateTimer(status,secondsGame)
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true
+                                     , block: { [weak self] (_) in
+            self?.secondsGame -= 1
+        })
     }
  
+    func newGame(){
+        status = .start
+        self.secondsGame = self.timerForGame
+        setupGame()
+    }
+    
     func check(index: Int){
-        
+        guard status == .start else {return}
         if itemsButtons[index].title == nextItem?.title {
             itemsButtons[index].isFound = true
             nextItem = itemsButtons.shuffled().first(where: { (item) -> Bool in
                 item.isFound == false
             })
+        } else {
+            itemsButtons[index].isError = true
         }
         if nextItem == nil {
             status = .win
         }
     }
+ 
+    private func stopGame() {
+        timer?.invalidate()
+    }
     
+}
+
+
+extension Int{
+    func secondsToString() -> String{
+        let minutes = self / 60
+        let seconds = self % 60
+        
+        return String(format: "%d:%02d", minutes,seconds)
+    }
 }
--- FindNumber_icodeschool/GameViewController.swift
+++ FindNumber_icodeschool/GameViewController.swift
@@ -1,33 +1,32 @@
-//
-//  GameViewController.swift
-//  FindNumber_icodeschool
-//
-//  Created by Maxim M on 25.01.2022.
-//
-
 import UIKit
 
 class GameViewController: UIViewController {
-//    @IBOutlet weak var firstButton: UIButton!
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet weak var nextDigit: UILabel!
-    @IBOutlet weak var statusLable: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var newGameButton: UIButton!
     
-    lazy var game = Game(countItems: buttons.count)
+    lazy var game = Game(countItems: buttons.count,time: 30) { [weak self] (status, seconds) in
+        guard let self = self else {return}
+        self.timeLabel.text = seconds.secondsToString()
+        self.updateInfoGame(with: status)
+    }
     
     @IBAction func pressButton(_ sender: UIButton) {
         guard let buttonIndex = buttons.firstIndex(of: sender) else {return}        //получаем индекс текущец кнопки
         game.check(index: buttonIndex)      //передаем в модель индекс кнопки, на которую нажал пользователь
         updateUI()
- //       sender.isHidden = true
- //       print(sender.currentTitle)
     }
-
+    @IBAction func pressNewGameButton(_ sender: UIButton) {
+        game.newGame()
+        sender.isHidden = true
+        setupScreen()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        firstButton.backgroundColor = .orange
-//        firstButton.setTitle("10", for: .normal)
         setupScreen()
     }
     
@@ -35,20 +34,49 @@ class GameViewController: UIViewController {
     private func setupScreen() {        // отображаем сгенерированные цифры из модели на кнопках экрана
         for i in game.itemsButtons.indices{
             buttons[i].setTitle(game.itemsButtons[i].title, for: .normal)
-            buttons[i].isHidden = false
+            //buttons[i].isHidden = false
+            buttons[i].alpha = 1
+            buttons[i].isEnabled = !game.itemsButtons[i].isFound
         }
         nextDigit.text = game.nextItem?.title
     }
     
     private func updateUI() {
         for i in game.itemsButtons.indices {
-            buttons[i].isHidden = game.itemsButtons[i].isFound
+         //   buttons[i].isHidden = game.itemsButtons[i].isFound
+            buttons[i].alpha = game.itemsButtons[i].isFound ? 0 : 1
+            buttons[i].isEnabled = !game.itemsButtons[i].isFound
+            
+            
+        //animation
+        if game.itemsButtons[i].isError{
+            UIView.animate(withDuration: 0.3) { [weak self] in     // длительность анимации
+                self?.buttons[i].backgroundColor = .red      //то что анимируется
+            } completion: { [weak self] (_) in                             //действия по окончании анимации
+                self?.buttons[i].backgroundColor = .white
+                self?.game.itemsButtons[i].isError = false
+            }
+            }
         }
+        
         nextDigit.text = game.nextItem?.title
-        if game.status == .win {
-            statusLable.text = "Win"
-            statusLable.textColor = .green
-        }
+        updateInfoGame(with: game.status)
     }
     
+    private func updateInfoGame(with status: StatusGame) {
+        switch status {
+        case .start:
+            statusLabel.text = "Find the number"
+            statusLabel.textColor = .black
+            newGameButton.isHidden = true
+        case .win:
+            statusLabel.text = "Win"
+            statusLabel.textColor = .green
+            newGameButton.isHidden = false
+        case .lose:
+            statusLabel.text = "Lose"
+            statusLabel.textColor = .red
+            newGameButton.isHidden = false
+        }
+    }
 }
